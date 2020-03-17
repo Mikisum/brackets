a63d9300d74ac07327ccdadbe9f5305581eb6129
Viktoryia Kiyanka <mikisum@mail.ru>
2020-03-17 13:24:01 +0300

    fix:  fix a bug in inplemented function

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
   if (str.length % 2 != 0) 
-  return false;
+    return false;
   
-  for (let i = 0; i < bracketsConfig.length; i++) {
+  for (let i = 0; i < bracketsConfig.length;) {
     let pair = bracketsConfig[i].join('');
       if (str.includes(pair)) {
         str = str.replace(pair,'');
         i = 0;
+      }
+      else {
+        i++
       }  
     }
     return (str.length === 0);
   }
-  
-
 
 
